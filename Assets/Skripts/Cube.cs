c0578eba8d7716cd08c0b0630705b487c6422cc1
bbshkins <kostyapk3@gmail.com>
2024-05-12 16:26:44 +0300

    Added Explode methode to Cube skript

--- Assets/Skripts/Cube.cs
+++ Assets/Skripts/Cube.cs
@@ -5,7 +5,8 @@ public class Cube : MonoBehaviour
 {
     [SerializeField] private float _explosionForce;
     [SerializeField] private float _explosionRadius;
-    [SerializeField] private Cube _cubePrefab;
+    [SerializeField] private Rigidbody _cube;
+    [SerializeField] private ParticleSystem _explosion;
 
     private int _minCloneValue = 2;
     private int _maxCloneValue = 6;
@@ -19,19 +20,35 @@ public class Cube : MonoBehaviour
         if (Random.value <= transform.localScale.x)
         {
             for (int i = 0; i < randomCloneValue; i++)
-                CreateClone().GetComponent<Rigidbody>().AddExplosionForce(_explosionForce, transform.position, _explosionRadius);
+                CreateClone();
+        }
+        else
+        {
+            Explode();
         }
 
         Destroy(gameObject);
     }
 
-    private Cube CreateClone()
+    private void CreateClone()
     {
-        var clone = Instantiate(_cubePrefab);
+        var clone = Instantiate(_cube);
 
         clone.transform.localScale = transform.localScale / 2;
         clone.GetComponent<Renderer>().material.color = Random.ColorHSV();
+        clone.AddExplosionForce(_explosionForce, transform.position, _explosionRadius);
+    }
 
-        return clone;
+    private void Explode()
+    {
+        float scaleBuff = 1 / transform.localScale.x;
+
+        foreach (Collider hit in Physics.OverlapSphere(transform.position, _explosionRadius))
+        {
+            if (hit.TryGetComponent<Cube>(out _))
+                hit.attachedRigidbody.AddExplosionForce(_explosionForce * scaleBuff, transform.position, _explosionRadius * scaleBuff);
+        }
+
+        Instantiate(_explosion, transform.position, Quaternion.identity);
     }
 }
